32146e0ff0e3da8b580e62df3ca8a06ae0096e24
eimwe <prokopenkoserge@inbox.ru>
2024-12-12 21:13:48 +0500

    Override to stop fetches if got redirected

--- src/scripts/FetchIntercept.js
+++ src/scripts/FetchIntercept.js
@@ -8,6 +8,9 @@ class FetchIntercept {
 
     // Flag to control fetch behavior
     this.isMockEnabled = true;
+
+    // Flag to block subsequent fetches
+    this.isRedirecting = false;
   }
 
   setup(interceptorFn) {
@@ -15,6 +18,11 @@ class FetchIntercept {
 
     // Override the global fetch method for ALL requests
     window.fetch = async (...args) => {
+      if (this.isRedirecting) {
+        console.warn("Fetch blocked due to ongoing redirect");
+        return new Response(null, { status: 403, statusText: "Fetch Blocked" });
+      }
+
       if (!this.isMockEnabled) {
         return this.originalFetch(...args);
       }
@@ -29,7 +37,18 @@ class FetchIntercept {
         if (interceptorResult instanceof Response) {
           const locationHeader = clonedResponse.headers.get("Location");
           if (locationHeader) {
+            this.isRedirecting = true;
             window.location.href = locationHeader;
+
+            // Prevent any further fetch requests
+            window.fetch = () => {
+              console.warn("Fetch blocked due to redirect");
+              return new Response(null, {
+                status: 403,
+                statusText: "Fetch Blocked",
+              });
+            };
+
             return interceptorResult;
             // all subsequent fetches must be invalid!
           }
@@ -80,12 +99,14 @@ class FetchIntercept {
 
   restore() {
     this.isMockEnabled = false;
+    this.isRedirecting = false;
     window.fetch = this.originalFetch;
     this.interceptor = null;
   }
 
   enableMock() {
     this.isMockEnabled = true;
+    this.isRedirecting = false;
   }
 }
 
